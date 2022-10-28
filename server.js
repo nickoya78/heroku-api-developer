d9484945d0589bc120672d812e50b75bbc738790
Nicholas Oyagh <you@example.com>
2022-10-28 14:56:59 -0700

    my first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// All my API code goes here
+app.get("/api", (req, res) => {
+  res.json({
+    message: "This is the api endpoint",
+  });
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port, (_) => {
+  console.log(`server is started on port ${port}`);
+});
