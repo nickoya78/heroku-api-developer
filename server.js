4b2c4b254004a246cdf601ad70cde8f3a0fb8d19
Nicholas Oyagh <you@example.com>
2022-10-28 17:03:55 -0700

    final changes

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ app.use(express.static(path.join(__dirname, "build")));
 // All my API code goes here
 app.get("/api", (req, res) => {
   res.json({
-    message: "This is the api endpoint",
+    users: [
+      "userOne: Alex Cooper, email: alex@gmail.com, phone: 626-400-9007",
+      "userTwo: David Smith, email: david@aol.com, phone: 322-300-5008",
+      "userThree: James Bond, email: james@yahoo.com, phone:818-214-4001",
+    ],
   });
 });
 
